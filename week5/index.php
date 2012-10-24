fe7532dfb2ebd65f8f2faa04c4975fa080c80716
Peter Tseng <patseng@stanford.edu>
2012-10-24 15:49:58 -0700

    week 5

--- week5/index.php
+++ week5/index.php
@@ -9,10 +9,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1"> 
 
 	<link rel="stylesheet" href="jquery.mobile-1.2.0.css" />
+	<link rel="stylesheet" href="themes/christmas.css" />
 
 	<link rel="stylesheet" href="style.css" />
 	<link rel="apple-touch-icon" href="appicon.png" />
-	<link rel="apple-touch-startup-image" href="upstart.png">
+	<link rel="apple-touch-startup-image" href="startup.png">
 	
 	<script src="jquery-1.8.2.min.js"></script>
 	<script src="jquery.mobile-1.2.0.js"></script>
